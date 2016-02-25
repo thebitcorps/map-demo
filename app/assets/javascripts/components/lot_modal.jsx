f108a8d4826246b71f7932df17dcc12d36948825
Diego Llamas <diegollams@gmail.com>
2016-02-25 12:15:13 -0600

    add lot info in modal missing fix styling of elemnt

--- app/assets/javascripts/components/lot_modal.jsx
+++ app/assets/javascripts/components/lot_modal.jsx
@@ -1,15 +1,16 @@
+/**
+ * reactProps: number:
+ */
 var LotModal = React.createClass({
     getDefaultProps: function(){
-        return {name: 'modal'}
+        return {name: 'modal',number: '0'}
     },
 
     openModal: function() {
-        //$('#'+this.props.name).modal('show');
         this.modal.modal('show');
     },
 
     closeModal: function() {
-        //$('#'+this.props.name).modal('hide');
         this.modal.modal('hide');
     },
     componentDidMount: function(){
@@ -23,7 +24,6 @@ var LotModal = React.createClass({
     render: function() {
         return (
             <div>
-                <button type="button" className="btn btn-default" onClick={this.openModal}>Open</button>
                 <div
                     id={this.props.name}
                     tabIndex='-1'
@@ -39,19 +39,26 @@ var LotModal = React.createClass({
                                     <span aria-hidden="true">&times;</span>
                                     <span className="sr-only">Close</span>
                                 </button>
-                                <h4 className="modal-title">Modal title</h4>
+                                <h3 className="modal-title">{'Lote ' + this.props.number + ' cuadra '+ this.props.block}</h3>
                             </div>
                             <div className="modal-body">
                                 <div className='row' style={{height: '90%'}}>
                                     <div className='col-sm-8' id='modal-div' style={{height: '90%'}}>
                                         <canvas id='canvas'></canvas>
                                     </div>
-                                    <div className='col-sm-4'>
-                                        <ul>
-                                            <li>Numero: </li>
-                                            <li>Cuadra: </li>
-                                            <li>Estado: </li>
-                                        </ul>
+                                    <div className="col-sm-4">
+                                        <div className="panel panel-success ">
+                                            <div className="panel-heading">Informacion general   </div>
+                                            <div className="panel-body">
+                                                <ul className="list-group">
+                                                    <li className="list-group-item">Numero de lote: {this.props.number}</li>
+                                                    <li className="list-group-item">Numero de cuadra:  {this.props.block}</li>
+                                                    <li className="list-group-item">Etapa:  {this.props.stage}</li>
+                                                    <li className="list-group-item">Metros cuadrado:  {this.props.square_meters} m2</li>
+                                                    <li className="list-group-item">Estado del lote: {this.props.status}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
 
